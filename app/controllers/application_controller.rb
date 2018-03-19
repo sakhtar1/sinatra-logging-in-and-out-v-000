243c5628b693949d3f7c2c65f8279eb19171a37a
Sharmina Akhtar <sakht318@gmail.com>
2018-03-19 18:13:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,8 +23,6 @@ class ApplicationController < Sinatra::Base
     @current_user = User.find_by_id(session[:user_id])
    if @current_user
      erb :account
-   else
-     erb :error
    end
   end
 
